db876cf3d63206f1164c0e463432c7fbb59ae1a8
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-09 11:37:07 +0530

    Add search engines to firefox

--- programs/browser.nix
+++ programs/browser.nix
@@ -63,6 +63,35 @@
       PrivateBrowsingModeAvailability = 2;
       PromptForDownloadLocation = true;
       SanitizeOnShutdown = true;
+      SearchEngines = {
+        Default = "ddg";
+        Add = [
+          {
+            Name = "nixpkgs packages";
+            URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@np";
+          }
+          {
+            Name = "NixOS options";
+            URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@no";
+          }
+          {
+            Name = "NixOS Wiki";
+            URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+            IconURL = "https://wiki.nixos.org/favicon.ico";
+            Alias = "@nw";
+          }
+          {
+            Name = "noogle";
+            URLTemplate = "https://noogle.dev/q?term={searchTerms}";
+            IconURL = "https://noogle.dev/favicon.ico";
+            Alias = "@ng";
+          }
+        ];
+      };
       SearchSuggestEnabled = true;
       ShowHomeButton = true;
       SkipTermsOfUse = true;
