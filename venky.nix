456fb78138f783f5f804e87d9e4de8cf13e6c375
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-11 12:37:19 +0530

    Install Zed editor

--- venky.nix
+++ venky.nix
@@ -5,6 +5,8 @@ in
 {
   home.stateVersion = "26.11";
 
+  imports = [ ./venky/zed.nix ];
+
   xdg.configFile."niri".source = mkSymlink ./configs/niri;
   xdg.configFile."nvim".source = mkSymlink ./configs/nvim;
   xdg.configFile."kitty".source = mkSymlink ./configs/kitty;
