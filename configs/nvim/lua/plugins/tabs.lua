39fd14c4c2294182e8650e12656176defbe55766
Venkatesh Ravipudi <venkateshravipudi555@gmail.com>
2026-07-11 16:04:11 +0530

    Refactor keymaps

--- configs/nvim/lua/plugins/tabs.lua
+++ configs/nvim/lua/plugins/tabs.lua
@@ -6,16 +6,16 @@ return {
 	},
 	init = function()
 		vim.g.barbar_auto_setup = false
-		local map = vim.keymap.set
-		map("n", "<A-j>", ":BufferPrevious<CR>", { desc = "Previous Buffer" })
-		map("n", "<A-k>", ":BufferNext<CR>", { desc = "Next Buffer" })
-		map("n", "<A-c>", ":BufferClose<CR>", { desc = "Close Current Buffer" })
+		vim.keymap.set("n", "<A-j>", ":BufferPrevious<CR>", { desc = "Previous Buffer" })
+		vim.keymap.set("n", "<A-k>", ":BufferNext<CR>", { desc = "Next Buffer" })
+		vim.keymap.set("n", "<A-c>", ":BufferClose<CR>", { desc = "Close Current Buffer" })
 		-- Sort automatically by...
-		map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", { desc = "Order Buffers by Buffer Numbers" })
-		map("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", { desc = "Order Buffers by Buffer Names" })
-		map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", { desc = "Order Buffers by Buffer Directories" })
-		map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", { desc = "Order Buffers by Buffer Languages" })
-		map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", { desc = "Order Buffers by Buffer Window Numbers" })
+		vim.keymap.set("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", { desc = "Order Buffers by Buffer Numbers" })
+		vim.keymap.set("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", { desc = "Order Buffers by Buffer Names" })
+		vim.keymap.set("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", { desc = "Order Buffers by Buffer Directories" })
+		vim.keymap.set("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", { desc = "Order Buffers by Buffer Languages" })
+		vim.keymap.set("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>",
+			{ desc = "Order Buffers by Buffer Window Numbers" })
 	end,
 	opts = {
 		animation = true,
